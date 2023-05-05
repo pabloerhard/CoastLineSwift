5fc194625343d30db2039b21a3fdf2c23f4d8043
Leonardo Gonzalez <leogzz0@hotmail.com>
2023-05-05 15:57:33 -0600

    hola

--- JUEGO/ContentView.swift
+++ JUEGO/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     Jugador(id: 0, nombre: "Nico", puntos: 13, email: "ad@gma.com"), Jugador(id: 1, nombre: "David", puntos: 2, email: "goe@out.com"), Jugador(id: 3, nombre: "pepe", puntos: 532, email: "fko@iclo.com")
     ]
     
+    // hola
+    
     var body: some View {
         NavigationView{
             List(jugadores){
