23feaa4bc260a29f15b6ff08a3997c94e88ba634
Alejandro <alexe@Alejandros-iMac.local>
2023-05-08 17:47:49 -0600

    firebase testing

--- JUEGO/Views/ContentView.swift
+++ JUEGO/Views/ContentView.swift
@@ -14,27 +14,35 @@ struct ContentView: View {
     var body: some View {
         
         GeometryReader {geo in
-            VStack(alignment: .center){
-                Text("Log In")
-                    .font(Font.custom("HelveticaNeue-Thin", size: 48))
-                TextField("Inserta Email", text: $email)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .border(Color.black)
-                    .keyboardType(.emailAddress)
-                    .frame(width: geo.size.width * 0.5)
-                TextField("Inserta Usuario", text: $password)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(15)
-                    .border(Color.black)
-                    .keyboardType(.default)
-                    .frame(width: geo.size.width * 0.5)
-                
+            VStack {
+                NavigationView {
+                    VStack(alignment: .center){
+                        Text("Log In")
+                            .font(Font.custom("HelveticaNeue-Thin", size: 48))
+                        TextField("Inserta Email", text: $email)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .border(Color.black)
+                            .keyboardType(.emailAddress)
+                            .frame(width: geo.size.width * 0.5)
+                        TextField("Inserta Usuario", text: $password)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(15)
+                            .border(Color.black)
+                            .keyboardType(.default)
+                            .frame(width: geo.size.width * 0.5)
+                        
+                        NavigationLink(destination:signUpView()){
+                            Text("Crear Cuenta")
+                        }
+                        
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .alignmentGuide(.leading) { _ in geo.size.height / 2 }
+                }
             }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .alignmentGuide(.leading) { _ in geo.size.height / 2 }
         }
 
         
